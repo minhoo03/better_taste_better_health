afc8d6ff6c19ba124604751c970cacafb9529cab
minhoo03 <minhoo0333@gmail.com>
2021-03-21 02:04:22 +0900

    jq slick slider plugin

--- js/index.js
+++ js/index.js
@@ -2,3 +2,20 @@
 $(document).ready(function(){
     $(".wrapper").smoothWheel()
 });
+
+let intro_des_img_img 
+let wrapper
+
+
+    intro_des_img_img = document.getElementsByClassName('intro_des_img_img')[0]
+    wrapper = document.getElementsByClassName('wrapper')[0]
+
+
+
+wrapper.addEventListener("scroll" , function() {
+    let scrollTop = document.documentElement.scrollTop;
+
+    intro_des_img_img.style.transform = `translate(0, ${scrollTop / 8}px)`
+
+    console.log(scrollTop)
+})
