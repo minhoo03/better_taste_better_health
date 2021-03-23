5980ad27a7e784051f79581a8fb206e7c219649d
minhoo03 <minhoo0333@gmail.com>
2021-03-23 17:38:30 +0900

    Interactive

--- js/index.js
+++ js/index.js
@@ -3,19 +3,23 @@ $(document).ready(function(){
     $(".wrapper").smoothWheel()
 });
 
-let intro_des_img_img 
-let wrapper
+let intro_des_img_img = $('.intro_des_img_img')
+
+let wrapper = document.getElementsByClassName('wrapper')[0]
+
+let intro_des_img = $('.intro_des_img')
+let intro_des_img_height = intro_des_img.height() // 박스 크기
+let intro_des_img_offset_top = intro_des_img.offset().top // 박스 위치
+let wrapper_scroll_top  // 현재 스크롤
 
 
-    intro_des_img_img = document.getElementsByClassName('intro_des_img_img')[0]
-    wrapper = document.getElementsByClassName('wrapper')[0]
-
-
+console.log('intro_des_img.height()',intro_des_img.height())
+console.log('intro_des_img.offset.top',intro_des_img.offset().top)
 
 wrapper.addEventListener("scroll" , function() {
-    let scrollTop = document.documentElement.scrollTop;
-
-    intro_des_img_img.style.transform = `translate(0, ${scrollTop / 8}px)`
-
-    console.log(scrollTop)
+    wrapper_scroll_top = $(wrapper).scrollTop() // 현재 스크롤
+    console.log(wrapper_scroll_top)
+    intro_des_img_img.css({
+        'transform': `translate(0px,${wrapper_scroll_top/15}px)`
+    })
 })
