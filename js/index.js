7942cf801787751107b739225f3e3ecdcc7ef45e
minhoo03 <minhoo0333@gmail.com>
2021-03-27 23:16:26 +0900

    main_recipe_finish

--- js/index.js
+++ js/index.js
@@ -15,7 +15,12 @@ let intro_des_img_img = $('.intro_des_img_img') // 1 img
 let about_img = $('.about_img') // 2
 let about_img_height = about_img.height() // 2 박스 크기
 let about_img_offset_top = about_img.offset().top // 2 박스 위치
-let about_img_img = $('.about_img_img')
+let about_img_img = $('.about_img_img') // 2 img
+
+let main_recipe = $('.main_recipe') // 3
+let main_recipe_height = main_recipe.height()
+let main_recipe_offset_top = main_recipe.offset().top
+let main_recipe_best_recipe = $('.best_recipe')
 
 let wrapper_scroll_top  // 현재 스크롤
 
@@ -30,6 +35,7 @@ wrapper.addEventListener("scroll" , function() {
     wrapper_scroll_top = $(wrapper).scrollTop() // 현재 스크롤
     intro_des_img_scroll = wrapper_scroll_top - (intro_des_img_offset_top - intro_des_img_height) -800 // 현재스크롤 - 박스위치 ( -100px -> 0px)
     about_img_scroll = wrapper_scroll_top - (about_img_offset_top - about_img_height) -800 // 현재스크롤 - 박스위치 ( -100px -> 0px)
+    main_recipe_scroll = wrapper_scroll_top - (main_recipe_offset_top - main_recipe_height) -800 // 현재스크롤 - 박스위치 ( -100px -> 0px)
 
     slideImg.css({
         'transform': `translate(0px,${wrapper_scroll_top/4}px)`
@@ -43,4 +49,7 @@ wrapper.addEventListener("scroll" , function() {
         'transform': `translate(0px, ${about_img_scroll/8}px)`
     })
     
+    main_recipe_best_recipe.css({
+        'transform': `translate(0px, ${-main_recipe_scroll/4}px)`
+    })
 })
